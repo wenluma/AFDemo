251a880356042330b22ad615d0e263afa1e76008
miaogaoliang <wenluma_521@163.com>
2015-02-27 22:24:33 +0800

    Initial Commit

---
+++ AFNetworkingManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFNetworkingManager
+//
+//  Created by family on 15/2/27.
+//  Copyright (c) 2015年 mgl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AFNetworkingManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNetworkingManager
+//
+//  Created by family on 15/2/27.
+//  Copyright (c) 2015年 mgl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
